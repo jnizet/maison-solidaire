d9d6898b72679519f19b2d167ae2c883a6b56623
jnizet <jb@ninja-squad.com>
2023-01-15 14:57:51 +0100

    feat: allow modifying responsibilities

--- src/app/responsibility/responsibility.routes.ts
+++ src/app/responsibility/responsibility.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ResponsibilitiesComponent } from './responsibilities/responsibilities.component';
+import { ResponsibilityEditionComponent } from './responsibility-edition/responsibility-edition.component';
 
 export const RESPONSIBILITY_ROUTES: Routes = [
   {
     path: '',
     component: ResponsibilitiesComponent
+  },
+  {
+    path: ':responsibilitySlug/edit',
+    component: ResponsibilityEditionComponent
   }
 ];
