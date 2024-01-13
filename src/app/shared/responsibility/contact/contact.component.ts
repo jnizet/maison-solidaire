22318cd56c9c32a80599329fe00d90559dcbe378
jnizet <jb@ninja-squad.com>
2024-01-13 09:29:56 +0100

    refactor: transform inputs into signal inputs

--- src/app/shared/responsibility/contact/contact.component.ts
+++ src/app/shared/responsibility/contact/contact.component.ts
@@ -1,8 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, Signal } from '@angular/core';
 import { Contact } from '../../contact.service';
 import * as icons from '../../../icon/icons';
 import { IconDirective } from '../../../icon/icon.directive';
-import { from } from 'rxjs';
 import { ToastService } from '../../../toast/toast.service';
 
 interface Channel {
@@ -20,62 +19,61 @@ interface Channel {
   styleUrls: ['./contact.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ContactComponent implements OnChanges {
-  @Input({ required: true }) contact!: Contact;
+export class ContactComponent {
+  contact = input.required<Contact>();
 
-  channels: Array<Channel> = [];
+  channels: Signal<Array<Channel>> = computed(() => {
+    const channels: Array<Channel> = [];
+    const contact = this.contact();
+    if (contact.email) {
+      channels.push({
+        icon: icons.email,
+        description: 'Par email\u00a0:',
+        href: `mailto:${contact.email}`,
+        label: contact.email
+      });
+    }
+    if (contact.phone) {
+      channels.push({
+        icon: icons.phone,
+        description: 'Par téléphone\u00a0:',
+        href: `tel:${contact.phone}`,
+        label: contact.phone
+      });
+    }
+    if (contact.mobile) {
+      channels.push({
+        icon: icons.mobile,
+        description: 'Par téléphone portable\u00a0:',
+        href: `tel:${contact.mobile}`,
+        label: contact.mobile
+      });
+    }
+    if (contact.whatsapp) {
+      channels.push({
+        icon: icons.whatsapp,
+        description: 'Par Whatsapp\u00a0:',
+        href: `https://wa.me/${this.whatsappNumber(contact.whatsapp)}`,
+        label: contact.whatsapp
+      });
+    }
+    return channels;
+  });
 
   icons = icons;
 
   constructor(private toastService: ToastService) {}
-
-  ngOnChanges(): void {
-    this.channels = [];
-    if (this.contact.email) {
-      this.channels.push({
-        icon: icons.email,
-        description: 'Par email\u00a0:',
-        href: `mailto:${this.contact.email}`,
-        label: this.contact.email
-      });
-    }
-    if (this.contact.phone) {
-      this.channels.push({
-        icon: icons.phone,
-        description: 'Par téléphone\u00a0:',
-        href: `tel:${this.contact.phone}`,
-        label: this.contact.phone
-      });
-    }
-    if (this.contact.mobile) {
-      this.channels.push({
-        icon: icons.mobile,
-        description: 'Par téléphone portable\u00a0:',
-        href: `tel:${this.contact.mobile}`,
-        label: this.contact.mobile
-      });
-    }
-    if (this.contact.whatsapp) {
-      this.channels.push({
-        icon: icons.whatsapp,
-        description: 'Par Whatsapp\u00a0:',
-        href: `https://wa.me/${this.whatsappNumber(this.contact.whatsapp)}`,
-        label: this.contact.whatsapp
-      });
-    }
-  }
 
   private whatsappNumber(whatsapp: string) {
     const regex = /\D/g;
     return whatsapp.replace(regex, '');
   }
 
-  copy(value: string) {
-    from(navigator.clipboard.writeText(value)).subscribe(() =>
-      this.toastService.display({
-        icon: icons.copied,
-        message: 'Copié dans le presse-papier'
-      })
-    );
+  async copy(value: string) {
+    await navigator.clipboard.writeText(value);
+    this.toastService.display({
+      icon: icons.copied,
+      message: 'Copié dans le presse-papier'
+    });
   }
 }
